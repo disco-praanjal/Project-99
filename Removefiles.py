c59069ba1c400906f32b5b0426175d9935c79f1f
disco-praanjal <84781900+disco-praanjal@users.noreply.github.com>
2022-05-13 14:53:46 +0530

    Add files via upload

---
+++ Removefiles.py
@@ -0,0 +1,32 @@
+import os
+import shutil
+
+os.system("date")
+
+The current date is: 13-05-2022
+Enter the new date: (dd-mm-yy)
+
+path = input("enter the name of the files to be cleared. ")
+
+files_to_clear = os.listfile(path)
+
+time.time(st_ctime)
+
+os.path.exists(path)
+
+if os.path.exists(path+'/'+ext):
+       shutil.move(path+'/'+file, path+'/'+ext+'/'+file)
+    else:
+        os.makedirs(path+'/'+ext)
+        shutil.move(path+'/'+file, path+'/'+ext+'/'+file)
+
+os.path.join()
+
+ctime = os.stat(path).st_ctime
+
+return ctime
+
+os.remove(path) 
+
+shutil.rmtree()
+
